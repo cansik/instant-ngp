f7cc150be9aa57687339caeca27da4d5487d8bfe
Florian <github@broox.ch>
2022-09-05 16:36:09 +0200

    added support for auto-training

--- process.ps1
+++ process.ps1
@@ -6,11 +6,18 @@ if (-not(Test-path "$path/transforms.json" -PathType leaf))
     python scripts/colmap2nerf.py --colmap_matcher sequential --run_colmap --aabb_scale $scale --images "$path"
 }
 
-
 # run testbed or renderer
 if ($render) {
     $render_name = Split-Path "$path" -Leaf
     python scripts/render.py --scene "$path" --n_seconds $time --fps $fps --render_name "$render_name" --width $width --height $height
 } else {
-    .\build\testbed.exe --scene "$path"
+	$add_args = ""
+	
+	if (Test-path "$path/base.msgpack" -PathType leaf)
+	{
+		echo "loading snapshot $path/base.msgpack"
+		python scripts/run.py --mode nerf --scene "$path" --load_snapshot "$path/base.msgpack" --near_distance 0.01 --gui
+	} else {
+		python scripts/run.py --mode nerf --scene "$path" --save_snapshot "$path/base.msgpack" --near_distance 0.01 --gui --train --n_steps 20000
+	}
 }
